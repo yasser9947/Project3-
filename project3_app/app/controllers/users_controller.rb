2c1101aef920109956e53db6810cc49818505521
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-21 16:03:23 +0300

    Modified Device Successfully

--- project3_app/app/controllers/users_controller.rb
+++ project3_app/app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
   user_signed_in?
 current_user
   end
   def home
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    puts params
+    @user = User.create(user_params)
+    redirect_to @user
+  end
+  def update
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :city)
+    end
 end
--- project3_app/db/schema.rb
+++ project3_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_091517) do
+ActiveRecord::Schema.define(version: 2019_04_21_115003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2019_04_21_091517) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "phone"
+    t.string "city"
+    t.string "nationality"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
