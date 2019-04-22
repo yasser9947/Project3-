7d8fe53b6ed39c8829ed3d19d2d1d8009f8caebc
Yasir S <yasser9946.1@gmail.com>
2019-04-22 11:34:19 +0300

    add care from model

--- project3_app/db/schema.rb
+++ project3_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_115003) do
+ActiveRecord::Schema.define(version: 2019_04_22_080921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_04_21_115003) do
     t.integer "phone"
     t.string "city"
     t.string "nationality"
+    t.boolean "care"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
