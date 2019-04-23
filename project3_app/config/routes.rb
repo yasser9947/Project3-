783ef177bdf7ed02afda615242771c92841b86e9
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-23 17:36:51 +0300

    Child and Senior Forum

--- project3_app/config/routes.rb
+++ project3_app/config/routes.rb
@@ -1,25 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :request
-
-
-  get 'requests/index'
-  get 'requests/new'
-  get 'requests/show'
-  get 'requests/edit'
-  get 'seniors/index'
-  get 'seniors/new'
-  get 'seniors/show'
-  get 'seniors/edit'
-  get 'childs/index'
-  get 'childs/new'
-  get 'childs/show'
-  get 'childs/edit'
-  devise_for :users
- get 'users/home'
- get 'users/show'
-
- resources :requests
+  resources :requests
    devise_scope :user do
     authenticated :user do
       root 'users#index', as: :authenticated_root
